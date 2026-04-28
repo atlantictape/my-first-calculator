cc13813e25adab89e534ac9898ca3c869871e3fc
atlantictape <lucas-mejia@hotmail.com>
2026-04-27 21:14:13 -0400

    Add files via upload

---
+++ calc.rs
@@ -0,0 +1,24 @@
+fn cubed(x: f64) -> f64 {
+
+	x*x*x
+}
+fn squared(x: f64) -> f64{
+	x*x
+}
+fn rooted(x: f64) -> f64{
+// genununly do ts later ngl
+}
+fn pathg(mut x: f64; mut y: f64) -> f64{
+
+	let hypotsq: f64;
+
+	hypotsq = squared(x)+squared(y);
+
+	let hypot = rooted(hypotsq);
+	
+	return hypot	
+}
+fn main(){
+
+
+}
